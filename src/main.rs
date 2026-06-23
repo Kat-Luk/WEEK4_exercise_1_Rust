271c8b9cdafd7340db85baea4ed3c4c9e53f7802
Kat-Luk <katarinalukacova6@gmail.com>
2026-06-23 21:05:13 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use std::thread;
+use std::io;
+use std::io::Write;
+
+fn main() {
+    loop {
+        io::stdout().flush().unwrap();
+        let mut user_input = String::new();
+        io::stdin().read_line(&mut user_input).expect("Failed to read input");
+        thread::spawn(move || {
+            match user_input.trim() {
+                "read" => read_file(),
+                "prank" => prank_user(),
+                _ => println!("Invalid command. Try again.")
+        }
+        });
+    }
+}
+
+fn read_file() {
+    println!("eww");
+}
+
+fn prank_user() {
+    println!("You have recived an email.");
+}
