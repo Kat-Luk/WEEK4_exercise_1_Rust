be4437f764bf9070db3fbbebb219f81a609f546b
Kat-Luk <katarinalukacova6@gmail.com>
2026-06-23 21:11:25 +0200

    Update command prompts to include '$' prefix for consistency

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,16 @@ use std::io;
 use std::io::Write;
 
 fn main() {
+    println!("$ Commands: read, prank, help, end.");
     loop {
         io::stdout().flush().unwrap();
         let mut user_input = String::new();
         io::stdin().read_line(&mut user_input).expect("Failed to read input");
         thread::spawn(move || {
             match user_input.trim() {
-                "read" => read_file(),
-                "prank" => prank_user(),
-                "end" => std::process::exit(0),
+                "$ read" => read_file(),
+                "$ prank" => prank_user(),
+                "$ end" => std::process::exit(0),
                 _ => println!("Invalid command. Try again.")
         }
         });
